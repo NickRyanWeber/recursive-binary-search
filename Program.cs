97c1fa501f86ea0c76752476ff77b0279254367a
nickryanweber <nicholasryanweber@gmail.com>
2019-09-09 16:04:56 -0400

    got recursion working

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace recursive_binary_search
+{
+  class Program
+  {
+    static void Count(int x)
+    {
+      if (x == 10)
+      {
+        return;
+      }
+      else
+      {
+        Console.WriteLine(x);
+        Count(x + 1);
+      }
+    }
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+
+      Count(0);
+    }
+  }
+}
